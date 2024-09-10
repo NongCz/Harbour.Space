e0a005182f769e8f37c339f5b4e0d61b385974a3
NongCz <Boonlerd2400@gmail.com>
2024-09-10 19:55:57 +0700

    Done

--- Day2.py
+++ Day2.py
@@ -1,1 +1,22 @@
-print("Hello, World")
+print("Hello, World") # in-line comment
+print("Hi:)", "NongChamp", "21 March", "2006", sep="\n")
+"""
+
+Multiple line1 
+Multiple line2 
+Multiple line3 
+
+Data Type
+
+integer-int                         1,0,-1,-2
+floating point-float                0.00,3.14,-2.6
+string-str                         "adadadad","word"
+boolean-bool                        True,False
+Nonetype-None
+
+"""
+print(type(0.00))
+print(isinstance(-1, int))
+print(isinstance(True, int))
+print(isinstance("A", int))
+print(type("A"))
