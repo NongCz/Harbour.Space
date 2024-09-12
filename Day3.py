b70ede81718f6d7a94705277add0e57c16b5f34c
NongCz <Boonlerd2400@gmail.com>
2024-09-12 16:49:05 +0700

    Done

---
+++ Day3.py
@@ -0,0 +1,13 @@
+print("What's your name?")
+name = input("Please write your name : ")
+print("Hello, " + name)
+age = input("How old are you : ")
+print("Wow, " + age + " too young")
+# reserved variables (should not use!)
+
+
+
+# Order of operations
+# () --> ** --> */% --> +-
+
+# a = a + b same a += b (Every operations)
