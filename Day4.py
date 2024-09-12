ec723406ddf4eb825af5a35ab1ea71c7b4c78f6a
NongCz <Boonlerd2400@gmail.com>
2024-09-12 19:42:02 +0700

    Done

---
+++ Day4.py
@@ -0,0 +1,44 @@
+# not, and, or
+# print(not True) => False
+
+# Adder Program
+
+"""
+
+first_number = int(input("Enter first number : "))
+second_number = int(input("Enter second number : "))
+# print(first_number + second_number)
+added_result = first_number + second_number
+print(added_result)
+
+"""
+
+# Change temperature units
+
+"""
+
+temperature_degree_celsius = float(input("Enter temperature in celsius unit : "))
+temperature_degree_fahrenheit = (temperature_degree_celsius * 9/5 + 32)
+temperature_degree_kelvin = (temperature_degree_celsius + 273.15)
+print(temperature_degree_fahrenheit,temperature_degree_kelvin)
+
+"""
+
+# Math functions => abs(), max(), min(), round()
+
+# Turtle Library
+# importing library
+import turtle
+turtle.speed(1)
+
+turtle.forward(200)
+turtle.left(90)  #turn left 90 degree
+turtle.forward(200)
+turtle.right(90) #turn right 90 degree
+turtle.color('blue')
+turtle.backward(200)
+turtle.circle(50, 360)
+turtle.width(3)
+turtle.color('Violet')
+turtle.backward(100)
+turtle.mainloop()
